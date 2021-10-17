9a539a21a4926a111d2349abb0b40059b703b319
Rupesh <rupeshsharma2196@gmail.com>
2021-10-17 09:14:51 +0530

    created required components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+import React, { useState } from "react";
 import styled from "styled-components";
+import Axios from "axios";
+import CityComponent from "./modules/CityComponent";
+import WeatherComponent from "./modules/WeatherInfoComponent";
+
+export const WeatherIcons = {
+	"01d": "/react-weather-app/icons/sunny.svg",
+	"01n": "/react-weather-app/icons/night.svg",
+	"02d": "/react-weather-app/icons/day.svg",
+	"02n": "/react-weather-app/icons/cloudy-night.svg",
+	"03d": "/react-weather-app/icons/cloudy.svg",
+	"03n": "/react-weather-app/icons/cloudy.svg",
+	"04d": "/react-weather-app/icons/perfect-day.svg",
+	"04n": "/react-weather-app/icons/cloudy-night.svg",
+	"09d": "/react-weather-app/icons/rain.svg",
+	"09n": "/react-weather-app/icons/rain-night.svg",
+	"10d": "/react-weather-app/icons/rain.svg",
+	"10n": "/react-weather-app/icons/rain-night.svg",
+	"11d": "/react-weather-app/icons/storm.svg",
+	"11n": "/react-weather-app/icons/storm.svg",
+};
 
 const Container = styled.div`
 	display: flex;
@@ -28,7 +49,25 @@ const CloseButton = styled.span`
 `;
 
 function App() {
-	return <div className="App">Hello</div>;
+	const [city, updateCity] = useState();
+	const [weather, updateWeather] = useState();
+	const fetchWeather = async (e) => {
+		e.preventDefault();
+		const response = await Axios.get(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`
+		);
+		updateWeather(response.data);
+	};
+	return (
+		<Container>
+			<AppLabel>React Weather App</AppLabel>
+			{city && weather ? (
+				<WeatherComponent weather={weather} city={city} />
+			) : (
+				<CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+			)}
+		</Container>
+	);
 }
 
 export default App;
