a3fb04f148efa468435b0ea2fa151e8884a75794
Rupesh <rupeshsharma2196@gmail.com>
2021-10-17 10:04:40 +0530

    modified update logic

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
 	const fetchWeather = async (e) => {
 		e.preventDefault();
 		const response = await Axios.get(
-			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6878e7f426d79a5c9c49feb95d1d5eba`
 		);
 		updateWeather(response.data);
 	};
